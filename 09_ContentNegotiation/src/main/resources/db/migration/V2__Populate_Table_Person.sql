01137878689b4be558231a02c369663648a64a80
DyaneAndrade <dyane.aaraujo@gmail.com>
2022-07-28 14:07:03 -0300

    Commit - Content Negotiation - JSON, XML E YAML

---
+++ 09_ContentNegotiation/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+(1,'Embu das Artes - SP','Dyane','Female','Andrade Araújo'),
+(2,'El Salvador','Lívia','Female','Andrade'),
+(4,'Taboão da Serra - SP','Davi','Male','Araújo'),
+(5,'Jardim Ângela - SP','Guilherme','Male','Raykow'),
+(6,'Embu das Artes - SP','Rafhael','Male','Andrade');
